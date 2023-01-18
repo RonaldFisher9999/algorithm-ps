d3edb78b2a20d360482832c8c77b51328f7b3b2a
RonaldFisher9999 <1993sabre@gmail.com>
2023-01-18 17:09:52 +0900

    23_01_18 hw

--- homework/week_01/홈워크_python_06_hw02_P.py
+++ homework/week_01/홈워크_python_06_hw02_P.py
@@ -4,13 +4,13 @@
 # B.    출력 예시 
 # [ ['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat'] ] 
 
-array = ['eat','tea','tan','ate','nat','bat']
+array = ['eatt','tea','tan','tate','nat','bat']
 
 anagram = [[array[0]]]
 n = len(array)
 for i in range(1, n) :
     for group in anagram :
-        if set(array[i]) == set(group[0]) :
+        if sorted(array[i]) == sorted(group[0]) :
             group.append(array[i])
             break
     else :

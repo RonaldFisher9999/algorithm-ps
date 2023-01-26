81ded53a0e1c55e8d47c4e442cdb34c8c9c8789d
RonaldFisher9999 <1993sabre@gmail.com>
2023-01-26 23:34:49 +0900

    23_01_26 algo

--- python_input.py
+++ python_input.py
@@ -4,15 +4,34 @@ sys.stdin = open('input.txt')
 # f = open("input.txt")
 # input = f.readline
 
-m, n = map(int, input().split())
-array = [list(map(int, input().split())) for _ in range(n)]
-target = int(input())
-cnt = 0
-for i in range(n) :
-    for j in range(m) :
-        if array[i][j] == target :
-            cnt += 1
-print(m, n)
-print(array)
-print(target)
-print(f"number of target in the array : {cnt}")
+n = int(input())
+m = int(input())
+edges = list()
+for _ in range(m) :
+    a, b, c = map(int, input().split())
+    edges.append((c, a, b))
+edges.sort()
+
+parent = list(range(n + 1))
+
+def find_par(x) :
+    if parent[x] != x :
+        parent[x] = find_par(parent[x])
+    return parent[x]
+
+def union(a, b) :
+    a = find_par(a)
+    b = find_par(b)
+    if a < b :
+        parent[b] = a
+    else :
+        parent[a] = b
+
+result = 0
+for edge in edges :
+    cost, a, b = edge
+    if find_par(a) != find_par(b) :
+        union(a, b)
+        result += cost
+
+print(result)

44b12a6bb24438b910064ed88ba412a65205347c
RonaldFisher9999 <1993sabre@gmail.com>
2023-01-30 16:33:20 +0900

    23_01_30 hw

--- homework/week_02/데일리_실습_python_06_5_P.py
+++ homework/week_02/데일리_실습_python_06_5_P.py
@@ -7,10 +7,18 @@ def sum_of_digit_1(num) :
     return answer
 
 # 반복문 활용 X
-def sum_fo_digit_2(num) :
+def sum_of_digit_2(num) :
     return sum(map(int, list(str(num))))
+
+# 재귀 활용
+def sum_of_digit_3(num) :
+    if num < 10 :
+        return num
+    return num % 10 + sum_of_digit_3(num // 10)
 
 nums = [34, 1435, 3904]
 
 for num in nums :
-    print(sum_of_digit_1(num), sum_fo_digit_2(num))
+    print(sum_of_digit_1(num))
+    print(sum_of_digit_2(num))
+    print(sum_of_digit_3(num))
